7e65c146f0b3e0b4ee038acd1f7b43a5fcba25db
Daniel Lande <danlande@gmail.com>
2018-01-12 18:39:35 -0700

    Removed references to ExampleSubsystem

--- RobotCode/src/org/usfirst/frc/team3216/robot/OI.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/OI.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team3216.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 
-import org.usfirst.frc.team3216.robot.commands.ExampleCommand;
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
--- RobotCode/src/org/usfirst/frc/team3216/robot/Robot.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/Robot.java
@@ -8,8 +8,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team3216.robot.commands.ExampleCommand;
-import org.usfirst.frc.team3216.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team3216.robot.subsystems.DriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,7 +19,7 @@ import org.usfirst.frc.team3216.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final DriveTrain exampleSubsystem = new DriveTrain();
 	public static OI oi;
 
 	Command autonomousCommand;
@@ -33,7 +32,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
+		// chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 	}
