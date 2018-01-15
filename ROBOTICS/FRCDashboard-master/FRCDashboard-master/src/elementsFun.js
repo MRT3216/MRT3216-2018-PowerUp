ded25502d58f41a9727320a8eec3191e09708abc
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2018-01-14 18:13:36 -0700

    Added match timer
    -Added match timer!

--- ROBOTICS/FRCDashboard-master/FRCDashboard-master/src/elementsFun.js
+++ ROBOTICS/FRCDashboard-master/FRCDashboard-master/src/elementsFun.js
@@ -34,3 +34,26 @@ function outputUpdate3(dead) {
     console.log(dead)
     document.querySelector('#dead').value = dead;
 }
+
+document.getElementById('timer').innerHTML =
+  02 + ":" + 30;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
