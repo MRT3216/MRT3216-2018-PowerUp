e224dc953adb2e8a4261cf6138e7231ede681bb3
CSGuy57 <dlande@mcpsmt.org>
2018-01-15 11:20:23 -0700

    Added entry to set JOYSTICK_DEADZONE

--- RobotCode/src/org/usfirst/frc/team3216/robot/RobotMap.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/RobotMap.java
@@ -46,7 +46,7 @@ public class RobotMap {
 	public static final Logger.Level	LOG_DRIVETRAIN			= Logger.Level.TRACE;
 	public static final Logger.Level	LOG_DRIVETRAIN_FOLLOWERS= Logger.Level.TRACE;
 	
-	public static double JOYSTICK_DEAD_ZONE = 0.05;
+	public static double JOYSTICK_DEADZONE = 0.05;
 	public static final double JOYSTICK_DRIVE_SCALE = 1.0;
 	
 	/** Network Tables **/
@@ -57,6 +57,6 @@ public class RobotMap {
 		NetworkTableInstance defaultTable =  NetworkTableInstance.getDefault();
 		NetworkTable settings = defaultTable.getTable(networkTableName);
 		
-		JOYSTICK_DEAD_ZONE = settings.getEntry(ntDeadzone);	
+		JOYSTICK_DEADZONE = settings.getEntry(ntDeadzone).getDouble(JOYSTICK_DEADZONE);	
 	}
 }
