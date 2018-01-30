323eb769ced05ca7ff3e290cb5d978e0dc226f56
Braeden Hunt <20hunbra@student.mcpsmt.org>
2018-01-29 20:28:56 -0700

    Added elevator_move
    didnt work tho.

--- RobotCode/src/org/usfirst/frc/team3216/robot/commands/Elevator_Move.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/commands/Elevator_Move.java
@@ -1,23 +1,32 @@
 package org.usfirst.frc.team3216.robot.commands;
 
+import org.usfirst.frc.team3216.robot.OI;
+import org.usfirst.frc.team3216.robot.Robot;
+import org.usfirst.frc.team3216.robot.subsystems.Elevator;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class Elevator_Move extends Command {
+	Elevator elevator = Robot.elevator;
+	OI oi = Robot.oi;
+
 
     public Elevator_Move() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(elevator);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	elevator.stop();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double power = oi.getStickY();
+    	elevator.setPower(power);
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- RobotCode/src/org/usfirst/frc/team3216/robot/subsystems/Elevator.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/subsystems/Elevator.java
@@ -4,7 +4,9 @@ import org.usfirst.frc.team3216.lib.Logger;
 import org.usfirst.frc.team3216.robot.OI;
 import org.usfirst.frc.team3216.robot.Robot;
 import org.usfirst.frc.team3216.robot.RobotMap;
+import org.usfirst.frc.team3216.robot.commands.Elevator_Move;
 
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -18,16 +20,30 @@ public class Elevator extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	OI oi = Robot.oi;
+	private Talon motor;
 	
 	public Elevator() {
-		
+		motor = new Talon(2);
 	}
 	
+	public void setPower(double power) {
+		power = safetyCheck(power);
+		motor.set(power);				
+	}
+	
+	public void stop() {
+		setPower(0.0);
+	}
+
+	private double safetyCheck(double power) {
+		power = Math.min(1.0, power);
+		power = Math.max(-1.0, power);
+		return power;
+	}
 	
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new Elevator_Move());
     }
 }
 
