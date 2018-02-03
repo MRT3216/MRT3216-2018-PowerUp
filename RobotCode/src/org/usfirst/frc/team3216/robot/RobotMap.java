efadb39aac797b13b4dbfcd060bc7f74f8a5eee9
CSGuy57 <dlande@mcpsmt.org>
2018-02-02 20:19:48 -0700

    Modified RobotMap to add TESTBOARD

--- RobotCode/src/org/usfirst/frc/team3216/robot/RobotMap.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/RobotMap.java
@@ -9,15 +9,15 @@ import org.usfirst.frc.team3216.lib.Logger;
  * floating around.
  */
 public class RobotMap {
-	public static enum Bot {BLUEFISH, MAINBOT;}
+	public static enum Bot {TESTBOARD, BLUEFISH, MAINBOT;}
 	public static enum Pincher {OPEN, CLOSED;}
 	public static enum Popper {EXTENDED, RETRACTED;}
 	public static enum Gear {HIGH, LOW;}	
 	public static Bot currentBot = Bot.BLUEFISH;
-	public static boolean hasIMU = false;//(currentBot == Bot.MAINBOT) ? false : true;
-	public static boolean hasJoystick = (currentBot == Bot.MAINBOT) ? false : true;
-	public static boolean hasPneumatics = (currentBot == Bot.MAINBOT) ? false : true;
-	public static boolean hasShifter = true;//(currentBot == Bot.MAINBOT) ? false : true;
+	public static boolean hasIMU = (currentBot == Bot.TESTBOARD) ? false : true;
+	public static boolean hasJoystick = (currentBot == Bot.BLUEFISH) ? false : true;
+	public static boolean hasPneumatics = (currentBot == Bot.BLUEFISH) ? false : true;
+	public static boolean hasShifter = (currentBot == Bot.BLUEFISH) ? false : true;
 	
 	/** Autonomous ************************************************************/
 	public static enum AutonomousModes {CROSS_LINE};
