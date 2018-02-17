abd9b84ef87e5a602a282c5e99beea58319f953a
CSGuy57 <danlande@gmail.com>
2018-02-17 16:09:06 -0700

    ?

--- RobotCode/src/org/usfirst/frc/team3216/robot/commands/Drivetrain_AutoProfileTest.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/commands/Drivetrain_AutoProfileTest.java
@@ -26,7 +26,7 @@ public class Drivetrain_AutoProfileTest extends Drivetrain_Drive {
   	
     	// 3 Waypoints    	
     	Waypoint[] points = new Waypoint[] {
-    	    //new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+    	    new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
     	    new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
     	    new Waypoint(0, 0, 0)                           // Waypoint @ x=0, y=0,   exit angle=0 radians
     	};
