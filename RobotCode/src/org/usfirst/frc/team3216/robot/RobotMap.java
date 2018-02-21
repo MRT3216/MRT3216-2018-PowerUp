d13b7fd4f4492eae2bd71e03aa8115f5ca73eb87
CSGuy57 <danlande@gmail.com>
2018-02-21 12:43:17 -0700

    Merged all robot map changes

--- RobotCode/src/org/usfirst/frc/team3216/robot/RobotMap.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/RobotMap.java
@@ -12,11 +12,12 @@ public class RobotMap {
 	public static enum Bot {TESTBOARD, BLUEFISH, MAINBOT;}
 	public static enum Pincher {OPEN, CLOSED;}
 	public static enum Popper {EXTENDED, RETRACTED;}
-	public static enum Gear {HIGH, LOW;}	
+	public static enum Gear {HIGH, LOW;}
+	public static enum cubeDetection {YES, NO;}
 	public static Bot currentBot = Bot.BLUEFISH;
-	
+
 	/** Subsystems for each bot ***********************************************/
-	public static boolean hasIMU = (currentBot == Bot.MAINBOT) ? false : true;
+	public static boolean hasIMU = (currentBot == Bot.MAINBOT) ? true : true;
 	public static boolean hasJoystick = (currentBot == Bot.BLUEFISH) ? false : true;
 	public static boolean hasPneumatics = (currentBot == Bot.BLUEFISH) ? false : true;
 	public static boolean hasShifter = (currentBot == Bot.BLUEFISH) ? false : true;
@@ -24,39 +25,37 @@ public class RobotMap {
 	public static boolean hasWinch = (currentBot == Bot.MAINBOT) ? true : false;
 	public static boolean hasElevator = (currentBot == Bot.MAINBOT) ? true : false;
 	public static boolean hasClimbArm = (currentBot == Bot.MAINBOT) ? true : false;
-	
+
 	/** Autonomous ************************************************************/
 	public static enum AutonomousModes {CROSS_LINE, SWITCH, SCALE};
 	public static enum StartingPositions {LEFT, CENTER, RIGHT};
-	
+
 	/** USB ports *************************************************************/
 	public static final int USB_GAMEPAD 		= 0;
 	public static final int USB_CONTROL_STICK 	= 2;
-	
+
 	/** PWM ID numbers ********************************************************/
 	public static final int PWM_LEFT_MOTOR 	= 0;
 	public static final int PWM_RIGHT_MOTOR = 1;
 	public static final int PWM_ELEVATOR_MOTOR = 2;
 	public static final int PWM_WINCH_MOTOR = 3;
 	public static final int PWM_CLIMB_ARM_MOTOR = 4;
-	
-	/** DIO ID numbers ********************************************************/
-	public static final int DIO_TOP_SWITCH = 0;
-	public static final int DIO_BOTTOM_SWITCH = 1;
-	
+
 	/** Analog Input ports ****************************************************/
 	public static final int RANGEFINDER = 0;
-	
+
 	/** Digital Input ports ***************************************************/
+	public static final int DIO_TOP_SWITCH = 0;
+	public static final int DIO_BOTTOM_SWITCH = 1;
 	public static final int LEFT_ENCODER_CHANNEL_A = 9;
 	public static final int LEFT_ENCODER_CHANNEL_B = 8;
 	public static final int RIGHT_ENCODER_CHANNEL_A = 7;
 	public static final int RIGHT_ENCODER_CHANNEL_B = 6;
-	
+
 	/** Drive Mode Settings ***************************************************/
 	public static enum DriveMode {TANK, ARCADE;}
 	public static DriveMode currentDriveMode = DriveMode.ARCADE;
-	
+
 	/** Drivetrain Settings ***************************************************/
 	public static final boolean REVERSE_LEFT_MOTOR = true;
 	public static final boolean REVERSE_RIGHT_MOTOR = false;
@@ -64,20 +63,20 @@ public class RobotMap {
 	// Determines the threshold at which the robot drives straight
 	public static final double METERS_TO_FEET = 3.28084;
 	public static final double TURN_RATE_THRESHOLD = 0.5;
-	public static final double DRIVETRAIN_GEAR_RATIO = 
+	public static final double DRIVETRAIN_GEAR_RATIO =
 			(currentBot == Bot.MAINBOT) ? 7.0 : 24.0/26;
-	public static final int DRIVETRAIN_ENCODER_PULSE_PER_REVOLUTION = 
+	public static final int DRIVETRAIN_ENCODER_PULSE_PER_REVOLUTION =
 			(currentBot == Bot.MAINBOT) ? 360 : 360;
-	public static final double WHEEL_DIAMETER = 
+	public static final double WHEEL_DIAMETER =
 			(currentBot == Bot.MAINBOT) ? 6 : 3.85;
-	public static final double WHEEL_DIAMETER_METERS = 
+	public static final double WHEEL_DIAMETER_METERS =
 			(currentBot == Bot.MAINBOT) ? 0.1524 : 0.098425;
 	public static final double WHEEL_DIAMETER_FEET = WHEEL_DIAMETER / 12.0;
 	public static final double WHEEL_WIDTH_METERS =
 			(currentBot == Bot.MAINBOT) ? 0.565 : 0.514;
 	public static final double WHEEL_WIDTH_FEET = WHEEL_WIDTH_METERS * METERS_TO_FEET;
 
-		
+
 	/** Logger Settings *******************************************************/
 	public static final String 		LOG_FILE_FORMAT = "yyyy-MM-dd-hhmmss";
 	public static final String 		LOG_TIME_FORMAT = "hh:mm:ss:SSS";
@@ -88,11 +87,11 @@ public class RobotMap {
 	public static final Logger.Level 	LOG_GLOBAL 				= Logger.Level.ALL;
 	public static final Logger.Level 	LOG_ROBOT 				= Logger.Level.OFF;
 	public static final Logger.Level	LOG_OI 					= Logger.Level.OFF;
-	
-	/** Subsystems **/                                                   
+
+	/** Subsystems **/
 	public static final Logger.Level	LOG_DRIVETRAIN			= Logger.Level.OFF;
 	public static final Logger.Level	LOG_RANGEFINDER  	    = Logger.Level.OFF;
-	public static final Logger.Level    LOG_IMU					= Logger.Level.OFF;	
+	public static final Logger.Level    LOG_IMU					= Logger.Level.OFF;
 	public static final Logger.Level    LOG_ENCODER             = Logger.Level.OFF;
 	public static final Logger.Level	LOG_PNEUMATICS			= Logger.Level.OFF;
 	public static final Logger.Level 	LOG_ELEVATOR 			= Logger.Level.OFF;
@@ -100,13 +99,13 @@ public class RobotMap {
 	public static final Logger.Level	LOG_WINCH				= Logger.Level.OFF;
 	public static final Logger.Level 	LOG_CLIMB_ARM			= Logger.Level.OFF;
 
-	
+
 	/** Commands **/
 	public static final Logger.Level LOG_DRIVEFORWARD			= Logger.Level.OFF;
 	public static final Logger.Level LOG_DRIVETRAIN_DRIVESTRAIGHT = Logger.Level.OFF;
 	public static final Logger.Level LOG_AUTOPROFILE			= Logger.Level.TRACE;
-	
-	
+
+
 	/** Network Table Key Names **/
 	public static final String networkTableName 				= "SmartDashboard";
 	// Control settings
@@ -122,11 +121,13 @@ public class RobotMap {
 	public static final String ntMedianSmoothingReadings		= "medianSmoothingReadings";
 	// Gyro
 	public static final String ntYaw							= "drive/navx/yaw";
-	// Robot Status	
+	// Robot Status
 	public static final String ntPincher						= "pincher";
 	public static final String ntPopper							= "popper";
 	public static final String ntElevatorHeight					= "elevatorHeight";
 	public static final String ntGear							= "gear";
+	// Cube detected -> Dashboard settings
+	public static final String ntCubeDetection					= "cubeDetection";
 	// Robot Settings
 	public static final String ntBot							= "bot";
 	public static final String ntClimbArmSpeed					= "climbArmSpeed";
@@ -150,15 +151,17 @@ public class RobotMap {
 	public static double JOYSTICK_SENSITIVITY 					= 0.5;
 	public static double DRIVESTRAIGHT_KP 						= 0.01;
 	// Rangefinder
-	public static int MEDIAN_SMOOTHING_READINGS					= 15;	
+	public static int MEDIAN_SMOOTHING_READINGS					= 15;
 	// Robot Status
 	public static Pincher PINCHER_STATUS						= RobotMap.Pincher.CLOSED;
 	public static Popper POPPER_STATUS							= RobotMap.Popper.RETRACTED;
 	public static double ELEVATOR_HEIGHT						= 0.0;
 	public static Gear CURRENT_GEAR                             = Gear.LOW;
+	//CubeDetection -> Dashboard Settings
+	public static Gear CURRENT_CUBEDETECTION                    = CUBEDETECTION.NO;
 	// Robot Settings
-	public static double CLIMB_ARM_SPEED						= 0.2;
-	public static double ELEVATOR_THRESHOLD						= 0.2;	
+	public static double CLIMB_ARM_SPEED						= 0.25;
+	public static double ELEVATOR_THRESHOLD						= 0.2;
 	// Autonomous
 	public static AutonomousModes AUTONOMOUS_MODE				= AutonomousModes.CROSS_LINE;
 	public static StartingPositions STARTING_POSITION			= StartingPositions.CENTER;
