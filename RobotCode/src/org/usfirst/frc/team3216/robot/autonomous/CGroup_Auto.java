09873516e20b509fe791fb6da9ea705761a46f41
Stupid-Steven <34665292+Stupid-Steven@users.noreply.github.com>
2018-04-05 12:21:09 -0600

    Added code to lift the elevator during Scale autonomous

--- RobotCode/src/org/usfirst/frc/team3216/robot/autonomous/CGroup_Auto.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/autonomous/CGroup_Auto.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3216.robot.autonomous;
 
 import org.usfirst.frc.team3216.robot.RobotMap;
 import org.usfirst.frc.team3216.robot.commands.CGroup_ShootCube;
+import org.usfirst.frc.team3216.robot.commands.Elevator_GoTo;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -12,6 +13,9 @@ public class CGroup_Auto extends CommandGroup {
 
 	public CGroup_Auto(String path) {
 		addSequential(new Drivetrain_AutoProfileDistanceFollowers(path));
+		if(path.contains("Scale")) {
+			addParallel(new Elevator_GoTo(true));
+		}
 		if (path != RobotMap.DRIVE_STRAIGHT) {
 			addSequential(new CGroup_ShootCube());
 		}
