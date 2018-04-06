6832dd5fa38d569e1ced94c227bf71c4c4dda05d
Stupid-Steven <34665292+Stupid-Steven@users.noreply.github.com>
2018-04-06 17:28:04 -0600

    Added code to check which side we are on to determine whether we should shoot the cube

--- RobotCode/src/org/usfirst/frc/team3216/robot/autonomous/CGroup_Auto.java
+++ RobotCode/src/org/usfirst/frc/team3216/robot/autonomous/CGroup_Auto.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team3216.robot.autonomous;
 
-import org.usfirst.frc.team3216.robot.RobotMap;
+import org.usfirst.frc.team3216.robot.OI;
+import org.usfirst.frc.team3216.robot.Robot;
 import org.usfirst.frc.team3216.robot.commands.CGroup_ShootCube;
-import org.usfirst.frc.team3216.robot.commands.Elevator_GoTo;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -14,11 +14,17 @@ public class CGroup_Auto extends CommandGroup {
 
 	public CGroup_Auto(String path) {
 		String gameData = DriverStation.getInstance().getGameSpecificMessage();
-		addSequential(new Drivetrain_AutoProfileDistanceFollowers(path));
-		if(path.contains("Scale")) {
-			addParallel(new Elevator_GoTo(true));
-		}
-		if (gameData.charAt(0) == 'R') {
+		
+		OI oi = Robot.oi;
+		
+		char pos = oi.getSide();
+		char switchPos = gameData.charAt(0);
+		
+		addSequential(new Drivetrain_AutoProfileDistanceFollowers(path));		
+
+		if (switchPos == 'R' && pos == 'R') {
+			addSequential(new CGroup_ShootCube());
+		} else if (switchPos == 'L' && pos == 'L') {
 			addSequential(new CGroup_ShootCube());
 		}
 	}
